1b9ebdb37b8bde496506247c8c1ea9fa62e7b963
fostrb <fostrb@gmail.com>
2018-03-11 23:21:27 -0400

    changed up project names specified x11 better brush color randomization probably other stuff

--- veil.py
+++ veil.py
@@ -6,15 +6,19 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, xlib, Gdk
 from gi.repository import GdkX11
-from math import pi
 import random
+from sys import platform as _platform
 
+if _platform != "linux" or _platform == "linux2":
+    sys.exit("No.")
+
+RANDOM_BRUSH_COLORS = True
 BRUSH_ALPHA = .5
 
 COLOR_GREEN = (0, 1, 0, BRUSH_ALPHA)
 COLOR_RED = (1, 0, 0, BRUSH_ALPHA)
 
-# Paneful; Fuck your virt windows all up
+# Veil; Fuck your x11 windows all up
 #
 # Python3.5
 #   gtk, cairo
@@ -26,9 +30,10 @@ COLOR_RED = (1, 0, 0, BRUSH_ALPHA)
 '''
 TODO:
 Never thread this. I'm not kidding.
+-Add more type hinting for python IDEs
 
 Functionality:
--Decide on toggling method
+-Tell Veil to toggle active from an external process (and hotkey that to a keypress)
 
 -DONE: Get signals propagating 'through' panes
 -SORTA DONE: Abstract and de-couple drawing before this stuff.
@@ -62,9 +67,10 @@ class Brush(object):
         self.stroke.append(point)
 
 
-class Paned(Gtk.Window):
-    def __init__(self, title='paned'):
-        super(Paned, self).__init__()
+class Veil(Gtk.Window):
+    def __init__(self, title='veil'):
+        super(Veil, self).__init__()
+        self.CURRENTLY_ACTIVE = True
         self.connect("destroy", Gtk.main_quit)
         self.brushes = []
         self.set_size_request(720, 480) #appears un-resizeable beneath these dimensions; find another function
@@ -81,8 +87,6 @@ class Paned(Gtk.Window):
         self.set_keep_above(True)
         self.connect('draw', self.draw_sig)
 
-        #self.set_resizable(True)
-        #self.set_accept_focus(False)
         #self.fullscreen()
 
         self.connect('motion-notify-event', self.mouse_move)
@@ -94,31 +98,28 @@ class Paned(Gtk.Window):
                         Gdk.EventMask.BUTTON_RELEASE_MASK)
         self.show()
 
-    # on draw...
-    # clear the screen
-    # do the things
-
-    def toggle_interact(self):
-        self.input_shape_combine_region(cairo.Region())
+    def toggle_active(self):
+        if self.CURRENTLY_ACTIVE:
+            self.input_shape_combine_region(cairo.Region())
+            self.CURRENTLY_ACTIVE = False
+        else:
+            self.input_shape_combine_region(None)
+            self.CURRENTLY_ACTIVE = True
 
     def draw_sig(self, widget, ctx):
         ctx.set_source_rgba(0, 0, 0, 0)
         ctx.rectangle(0, 0, *widget.get_size())
         ctx.set_operator(cairo.OPERATOR_CLEAR)
         ctx.fill()
-
         ##draw shit here
         ctx.set_operator(cairo.OPERATOR_OVER)
-        self.draw_border(ctx)
-        #self.draw_header(ctx)
-
-        #ctx.set_source_rgba(0, 1, 0, .2)
-        #ctx.arc(widget.get_size()[0] / 2, widget.get_size()[1] / 2, widget.get_size()[0] / 2, 0, pi * 2)
-        ctx.fill()
-
         self.brush_draw(ctx)
+        if self.CURRENTLY_ACTIVE:
+            self.draw_border(ctx)
+            self.draw_header(ctx)
 
     def draw_border(self, ctx):
+        ctx.set_line_width(2)
         ctx.set_source_rgb(0, 1, 0)
         ctx.rectangle(0, 0, *self.get_size())
         ctx.stroke()
@@ -132,8 +133,8 @@ class Paned(Gtk.Window):
             ctx.set_source_rgb(0, 0, 0)
             ctx.select_font_face("Inconsolata", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
             ctx.set_font_size(16)
-            ctx.move_to(0, 10)
-            ctx.show_text("pane")
+            ctx.move_to(0, 13)
+            ctx.show_text("Veil")
             ctx.stroke()
 
     def brush_draw(self, cr):
@@ -156,8 +157,10 @@ class Paned(Gtk.Window):
 
     def mouse_press(self, widget, event):
         if event.button == Gdk.BUTTON_PRIMARY:
-            #brush = Brush(10, COLOR_RED)
-            brush = Brush(10, (random.random(), random.random(), random.random(), 0.5))
+            if RANDOM_BRUSH_COLORS:
+                brush = Brush(10, (random.random(), random.random(), random.random(), 0.5))
+            else:
+                brush = Brush(10, COLOR_RED)
             brush.add_point((event.x, event.y))
             self.brushes.append(brush)
             widget.queue_draw()
@@ -171,6 +174,6 @@ class Paned(Gtk.Window):
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    exit_status = Paned()
+    exit_status = Veil()
     Gtk.main()
     sys.exit(exit_status)
